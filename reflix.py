c5d174a5bf91c9d1e53a634d4ec4120cf366e7ec
nexovir <nexoovir@gmail.com>
2025-09-18 23:32:23 +0330

    support dom sinks/sources

--- reflix.py
+++ reflix.py
@@ -9,9 +9,160 @@ green = '\033[92m'
 blue = '\033[94m'
 cyan = '\033[34m'
 yellow = '\033[33m'
+red = '\033[91m'
 reset = '\033[0m'
 
+DOM_SOURCES_AND_SINKS = {
+    'Common-Sources' : 
+        [
+        'document.URL',
+        'document.documentURI',
+        'document.URLUnencoded',
+        'document.baseURI',
+        'document.cookie',
+        'document.referrer',
+        'window.name',
+        'history.pushState',
+        'history.replaceState',
+        'localStorage',
+        'sessionStorage',
+        'IndexedDB',
+        'Database',
+        ],
 
+     'DOM-XSS-Sinks': 
+        ['document.write(',
+        'document.writeln(',
+        'document.domain',
+        '.innerHTML',
+        '.outerHTML',
+        '.insertAdjacentHTML',
+        '.onevent',
+        ],
+
+     'Open-Redirection-Sinks': 
+        [
+            'location',
+            'location.host',
+            'location.hostname',
+            'location.href',
+            'location.pathname',
+            'location.search',
+            'location.protocol',
+            'location.assign(',
+            'location.replace(',
+            'open(',
+            'element.srcdoc',
+            'XMLHttpRequest.open(',
+            'XMLHttpRequest.send(',
+            'jQuery.ajax(',
+            '$.ajax(',
+        ],
+
+     'Cookie-Manipulation-Sink':
+        [
+            'document.cookie'
+        ],
+
+     'JavaScript-Injection-Sinks':
+        [
+            'eval(',
+            'Function(',
+            'setTimeout(',
+            'setInterval(',
+            'setImmediate(',
+            'execCommand(',
+            'execScript(',
+            'msSetImmediate(',
+            'range.createContextualFragment(',
+            'crypto.generateCRMFRequest(',
+        ],
+
+     'WebSocket-URL-Poisoning-Sink':
+        [
+        'WebSocket'
+        ],
+
+    'Link-Manipulation-Sinks':
+        ['element.href',
+        'element.src',
+        'element.action',
+        ],
+
+    'Ajax-Request-Header-Manipulation-Sinks':
+        [
+            'XMLHttpRequest.setRequestHeader(',
+            'XMLHttpRequest.open(',
+            'XMLHttpRequest.send(',
+            'jQuery.globalEval(',
+            '$.globalEval(',
+        ],
+    'Local-File-Path-Manipulation-Sinks':
+        [
+            'FileReader.readAsArrayBuffer(',
+            'FileReader.readAsBinaryString(',
+            'FileReader.readAsDataURL(',
+            'FileReader.readAsText(',
+            'FileReader.readAsFile(',
+            'FileReader.root.getFile(',
+        ],
+
+    'Client-Side-SQL-Injection-Sink':
+        [
+        'executeSql('
+        ],
+    
+    'HTML5-Storage-Manipulation-Sinks':
+        [
+            'sessionStorage.setItem(',
+            'localStorage.setItem(',
+        ],
+
+    'XPath-Injection-Sinks':
+        [
+            'document.evaluate(',
+            'element.evaluate(',
+        ],
+    
+    'Client-Side-JSON-Injection-Sinks':
+        [
+            'JSON.parse(',
+            'jQuery.parseJSON(',
+            '$.parseJSON(',
+        ],
+
+    'DOM-Data-Manipulation-Sinks':
+        [
+        'script.src',
+        'script.text',
+        'script.textContent',
+        'script.innerText',
+        'element.setAttribute()',
+        'element.search',
+        'element.text',
+        'element.textContent',
+        'element.innerText',
+        'element.outerText',
+        'element.value',
+        'element.name',
+        'element.target',
+        'element.method',
+        'element.type',
+        'element.backgroundImage',
+        'element.cssText',
+        'element.codebase',
+        'document.title',
+        'document.implementation.createHTMLDocument(',
+        'history.pushState(',
+        'history.replaceState(',
+        ],
+    
+    'Denial-Of-Service-Sinks':
+        [
+            'requestFileSystem(',
+            'RegExp(',
+        ],
+}
 def show_banner():
     banner = pyfiglet.figlet_format("Reflix")
     twitter = Style.BRIGHT + Fore.CYAN + "X.com: @nexovir" + Style.RESET_ALL
@@ -66,6 +217,7 @@ configue_group.add_argument('-x', '--proxy', help='HTTP proxy to use (e.g., http
 configue_group.add_argument('-c', '--chunk', help='Number of URLs to process per batch (default: 25)',type=str,  default='25', required=False)
 configue_group.add_argument('-he', '--heavy', help='If enabled, it re-fuzzes all discovered parameters after light completes (default: False)',action='store_true',  default=False, required=False)
 configue_group.add_argument('-hd','--headless', help='Use headless browser (Playwright) to render full DOM and check reflections', action='store_true', default=False)
+configue_group.add_argument('-sd','--dom',help='Render pages with Playwright (headless) to execute JS and detect runtime sources/sinks (history.state, localStorage, IndexedDB). Slower but finds dynamic reflections.', action='store_true', default=False)
 
 
 # --- Injection Types ---
@@ -117,7 +269,7 @@ if args.headers:
 proxy = args.proxy
 chunk = args.chunk
 heavy = args.heavy
-
+dom = args.dom
 
 
 #Ratelimit Option
@@ -145,7 +297,7 @@ json_output = args.jsonoutput
 headless=args.headless
 
 def read_write_list(list_data: list, file: str, type: str):
-
+    
     objects = []
     
     if type == "read" or type == 'r':
@@ -390,20 +542,73 @@ def run_x8(url, parameters, proxy, thread, delay, method, headers, chunk , param
         sendmessage(f"[ERROR] Error in run_x8 with URL {url}: {str(e)}", colour="RED" , logger=logger , silent=silent)
         return []
 
+def explore_dom_sinks(url, proxy , thread , delay , headers , method):
+
+    sendmessage(f"  [INFO] Starting DOM sinks/sources exploration url : {url}" , colour="YELLOW" , logger=logger , silent=silent)
+
+    try:
+        with sync_playwright() as p:
+            browser = p.chromium.launch(
+                headless=True,
+                args=[
+                    "--no-sandbox",
+                    "--disable-gpu",
+                    "--disable-dev-shm-usage",
+                ],
+                proxy={"server": proxy} if proxy else None
+            )
+
+            context = browser.new_context(
+                user_agent=(
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                    "AppleWebKit/537.36 (KHTML, like Gecko) "
+                    "Chrome/120.0.0.0 Safari/537.36"
+                ),
+                extra_http_headers=headers or {},
+                ignore_https_errors=True,
+                viewport={"width": 1920, "height": 1080}
+            )
+            page = context.new_page()
+
+            response = page.goto(url, wait_until="networkidle")
+
+            
+            html = page.content()
+
+            browser.close()
+
+            for category, items in DOM_SOURCES_AND_SINKS.items() : 
+                sinks = []
+                for item in items:
+                    if item in html.lower():
+                        sinks.append(item.replace('(',''))
+                if sinks : 
+                    output_line = f"[{green}{method.upper()}{reset}] [{blue}http{reset}] [{cyan}info{reset}] [{yellow}{category}: {red}{str(sinks).replace('\'','')}{reset}] {url}"
+                    print(output_line)
+                    read_write_list([output_line], output, 'a')
+
+    except Exception as e:
+        sendmessage(f"[ERROR] Playwright scan failed: {str(e)}", colour="RED", logger=logger, silent=silent)
+        return {"success": False, "url": url, "error": str(e)}
+
+
 
 def light_reflix (urls, proxy, thread, delay, methods):
 
     sendmessage("[INFO] Starting Light Reflix ...", colour="YELLOW" , logger=logger , silent=silent)
     for url in urls :
+        if dom :
+            explore_dom_sinks (url, proxy , thread ,delay , headers , 'GET')
+
         for method in methods:
-            
             parameters = run_fallparams(url, proxy, thread, delay, method, headers)
             
             # save paramters output for client
             read_write_list(parameters , params_output , 'a')
 
             run_x8(url , parameters,  proxy , thread , delay , method, headers, chunk , parameter)
-            
+
+
 
 def heavy_reflix (urls , proxy , thread , delay , methods) : 
     
@@ -498,8 +703,8 @@ def path_injection_reflix(urls, proxy, thread, delay, methods, parameter, header
 
 
 
-def path_injection_reflix(urls, proxy, thread, delay, methods , parameter , headers , output):
-    print('hi')
+def header_injection_reflix(urls, proxy, thread, delay, methods , parameter , headers , output):
+    pass
 
 
 def main():
@@ -507,17 +712,17 @@ def main():
         show_banner() if not silent else None
         urls = read_write_list("", urls_path, 'r')
 
-        static_reflix (urls_path, generate_mode ,value_mode ,parameter , wordlist_parameters , chunk , proxy)
+        # static_reflix (urls_path, generate_mode ,value_mode ,parameter , wordlist_parameters , chunk , proxy)
         light_reflix(urls, proxy, thread, delay, methods)
         
-        if pathinjection:
-            path_injection_reflix(urls, proxy, thread, delay, methods , parameter , headers , output)
+        # if pathinjection:
+        #     path_injection_reflix(urls, proxy, thread, delay, methods , parameter , headers , output)
 
-        if headerinjection:
-            path_injection_reflix(urls, proxy, thread, delay, methods , parameter , headers , output)
+        # if headerinjection:
+        #     header_injection_reflix(urls, proxy, thread, delay, methods , parameter , headers , output)
 
-        if heavy : 
-            heavy_reflix(urls , proxy , thread , delay , methods)
+        # if heavy : 
+        #     heavy_reflix(urls , proxy , thread , delay , methods)
 
     except KeyboardInterrupt:
         sendmessage(
